471d92371e9dec8a3e8c64efecaca84ab43c09a4
hoquocnam92 <hoquocnam92@gmail.com>
2024-11-07 23:22:06 +0700

    add233

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Store_Books/', // Chỉ cần tên của repository
-})
+  base: '/Book_Store/', // Đảm bảo chỉ sử dụng tên repository, không phải URL đầy đủ
+});
