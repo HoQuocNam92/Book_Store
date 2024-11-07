a8d898c32225f898baf688bc01d1bb9d4bf378ed
hoquocnam92 <hoquocnam92@gmail.com>
2024-11-07 23:02:06 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/HoQuocNam92/Store_Books",
+  base: '/Store_Books/', // Chỉ cần tên của repository
 })
